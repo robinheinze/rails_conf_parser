daffe7a85150aca4ea4cff4dbee3e4aabbdc5443
Robin Miller <RMiller@crowdcompass.com>
2015-04-07 21:19:26 -0700

    clean up speaker keys

--- rails_conf_parser.rb
+++ rails_conf_parser.rb
@@ -20,11 +20,16 @@ parsed_sessions = raw_sessions.map do |session|
   {}.tap do |hash|
     hash[:unique_id] = session.css('a').first['name']
     hash[:name] = session.css('h2.session-talk-title a').text.strip
-    hash[:speaker_1_display_name] = session.css('h3.session-presenter').text.gsub('presented by: ', '')
+
     speaker_bio_pgraphs = session.css('section.bio p')
     description_pgraphs = session.css('p') - speaker_bio_pgraphs
+
     hash[:description] = description_pgraphs.map(&:to_s).join(' ')
-    hash[:speaker_1_bio] = speaker_bio_pgraphs.map(&:to_s).join(' ')
+    hash[:speaker] = {}.tap do |s| 
+      s[:display_name] = session.css('h3.session-presenter').text.gsub('presented by: ', '')
+      s[:bio] = speaker_bio_pgraphs.map(&:to_s).join(' ')
+    end
   end
 end
+
 pp parsed_sessions 
