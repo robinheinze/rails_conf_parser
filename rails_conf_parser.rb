3c85244395b58870f97cad8133c940824ba49cb6
Robin Miller <RMiller@crowdcompass.com>
2015-04-07 21:06:01 -0700

    parse session name, description, and speaker info from http://railsconf.com/program

--- rails_conf_parser.rb
+++ rails_conf_parser.rb
@@ -14,6 +14,17 @@ class Page
 end
 
 html_doc = Page.get('http://railsconf.com/program')
-activity_names = html_doc.css("h2.session-talk-title a")
 
-pp activity_names.map { |name| name.text.strip }
+raw_sessions = html_doc.css('div.session')
+parsed_sessions = raw_sessions.map do |session|
+  {}.tap do |hash|
+    hash[:unique_id] = session.css('a').first['name']
+    hash[:name] = session.css('h2.session-talk-title a').text.strip
+    hash[:speaker_1_display_name] = session.css('h3.session-presenter').text.gsub('presented by: ', '')
+    speaker_bio_pgraphs = session.css('section.bio p')
+    description_pgraphs = session.css('p') - speaker_bio_pgraphs
+    hash[:description] = description_pgraphs.map(&:to_s).join(' ')
+    hash[:speaker_1_bio] = speaker_bio_pgraphs.map(&:to_s).join(' ')
+  end
+end
+pp parsed_sessions 
